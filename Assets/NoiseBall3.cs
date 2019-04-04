efa8d1e7281b3df91d322219f196f8573fb87e81
Keijiro Takahashi <keijiro@gmail.com>
2019-04-04 16:50:04 +0900

    Initial commit

---
+++ Assets/NoiseBall3.cs
@@ -0,0 +1,139 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+public class NoiseBall3 : MonoBehaviour
+{
+    #region Editable attributes
+
+    [SerializeField] int _triangleCount = 100;
+
+    public int triangleCount {
+        get { return _triangleCount; }
+        set { _triangleCount = value; }
+    }
+
+    [SerializeField] float _triangleExtent = 0.1f;
+
+    public float triangleExtent {
+        get { return _triangleExtent; }
+        set { _triangleExtent = value; }
+    }
+
+    [SerializeField] float _shuffleSpeed = 4;
+
+    public float shuffleSpeed {
+        get { return _shuffleSpeed; }
+        set { _shuffleSpeed = value; }
+    }
+
+    [SerializeField] float _noiseAmplitude = 1;
+
+    public float noiseAmplitude {
+        get { return _noiseAmplitude; }
+        set { _noiseAmplitude = value; }
+    }
+
+    [SerializeField] float _noiseFrequency = 1;
+
+    public float noiseFrequency {
+        get { return _noiseFrequency; }
+        set { _noiseFrequency = value; }
+    }
+
+    [SerializeField] Vector3 _noiseMotion = Vector3.up;
+
+    public Vector3 noiseMotion {
+        get { return _noiseMotion; }
+        set { _noiseMotion = value; }
+    }
+
+    [SerializeField] Color _color = Color.white;
+
+    public Color color {
+        get { return _color; }
+        set { _color = value; }
+    }
+
+    [SerializeField, Range(0, 1)] float _metallic = 0.5f;
+
+    public float metallic {
+        get { return _metallic; }
+        set { _metallic = value; }
+    }
+
+    [SerializeField, Range(0, 1)] float _smoothness = 0.5f;
+
+    public float smoothness {
+        get { return _smoothness; }
+        set { _smoothness = value; }
+    }
+
+    #endregion
+
+    #region Hidden attribute
+
+    [SerializeField, HideInInspector] Shader _shader = null;
+
+    #endregion
+
+    #region Private variables
+
+    Material _material;
+
+    #endregion
+
+    #region MonoBehaviour functions
+
+    void OnValidate()
+    {
+        _triangleCount = Mathf.Max(0, _triangleCount);
+        _triangleExtent = Mathf.Max(0, _triangleExtent);
+        _noiseFrequency = Mathf.Max(0, _noiseFrequency);
+    }
+
+    void OnDestroy()
+    {
+        if (_material != null)
+        {
+            if (Application.isPlaying)
+                Destroy(_material);
+            else
+                DestroyImmediate(_material);
+        }
+    }
+
+    void Update()
+    {
+        if (_material == null)
+        {
+            _material = new Material(_shader);
+            _material.hideFlags = HideFlags.DontSave;
+        }
+
+        var time = Application.isPlaying ? Time.time : 3.3333f;
+
+        _material.SetColor("_Color", _color);
+        _material.SetFloat("_Metallic", _metallic);
+        _material.SetFloat("_Smoothness", _smoothness);
+
+        _material.SetFloat("_LocalTime", time * _shuffleSpeed);
+        _material.SetFloat("_Extent", _triangleExtent);
+        _material.SetFloat("_NoiseAmplitude", _noiseAmplitude);
+        _material.SetFloat("_NoiseFrequency", _noiseFrequency);
+        _material.SetVector("_NoiseOffset", _noiseMotion * time);
+
+        _material.SetMatrix("_LocalToWorld", transform.localToWorldMatrix);
+        _material.SetMatrix("_WorldToLocal", transform.worldToLocalMatrix);
+
+        Graphics.DrawProcedural(
+            _material,
+            new Bounds(transform.position, transform.lossyScale * 5),
+            MeshTopology.Triangles, _triangleCount * 3, 1,
+            null, null,
+            ShadowCastingMode.TwoSided, true, gameObject.layer
+        );
+    }
+
+    #endregion
+}
---
+++ Assets/NoiseBall3.shader
@@ -0,0 +1,119 @@
+Shader "Hidden/NoiseBall3"
+{
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+
+        Cull Off
+
+        CGPROGRAM
+
+        #pragma surface surf Standard vertex:vert addshadow
+        #pragma target 3.0
+
+        #include "UnityCG.cginc"
+        #include "SimplexNoise3D.cginc"
+
+        // Hash function from H. Schechter & R. Bridson, goo.gl/RXiKaH
+        uint Hash(uint s)
+        {
+            s ^= 2747636419u;
+            s *= 2654435769u;
+            s ^= s >> 16;
+            s *= 2654435769u;
+            s ^= s >> 16;
+            s *= 2654435769u;
+            return s;
+        }
+
+        // Random number (0-1)
+        float Random(uint seed)
+        {
+            return float(Hash(seed)) / 4294967295.0; // 2^32-1
+        }
+
+        // Random point on unit sphere
+        float3 RandomPoint(uint seed)
+        {
+            float u = Random(seed * 2 + 0) * UNITY_PI * 2;
+            float z = Random(seed * 2 + 1) * 2 - 1;
+            return float3(float2(cos(u), sin(u)) * sqrt(1 - z * z), z);
+        }
+
+        struct appdata
+        {
+            float4 vertex : POSITION;
+            float3 normal : NORMAL;
+            float4 tangent : TANGENT;
+            float4 texcoord1 : TEXCOORD1;
+            float4 texcoord2 : TEXCOORD2;
+            uint vid : SV_VertexID;
+        };
+
+        struct Input
+        {
+            float vface : VFACE;
+        };
+
+        half4 _Color;
+        half _Smoothness;
+        half _Metallic;
+
+        float _LocalTime;
+        float _Extent;
+        float _NoiseAmplitude;
+        float _NoiseFrequency;
+        float3 _NoiseOffset;
+
+        float4x4 _LocalToWorld;
+        float4x4 _WorldToLocal;
+
+        void vert(inout appdata v)
+        {
+            uint t_idx = v.vid / 3;         // Triangle index
+            uint v_idx = v.vid - t_idx * 3; // Vertex index
+
+            // Random triangle on unit sphere
+            uint seed = ((uint)_LocalTime + t_idx) * 3;
+            float3 v1 = RandomPoint(seed + 0);
+            float3 v2 = RandomPoint(seed + 1);
+            float3 v3 = RandomPoint(seed + 2);
+
+            // Constraint with the extent parameter
+            v2 = normalize(v1 + normalize(v2 - v1) * _Extent);
+            v3 = normalize(v1 + normalize(v3 - v1) * _Extent);
+
+            // Displacement by noise field
+            float l1 = snoise(v1 * _NoiseFrequency + _NoiseOffset).w;
+            float l2 = snoise(v2 * _NoiseFrequency + _NoiseOffset).w;
+            float l3 = snoise(v3 * _NoiseFrequency + _NoiseOffset).w;
+
+            l1 = abs(l1 * l1 * l1);
+            l2 = abs(l2 * l2 * l2);
+            l3 = abs(l3 * l3 * l3);
+
+            v1 *= 1 + l1 * _NoiseAmplitude;
+            v2 *= 1 + l2 * _NoiseAmplitude;
+            v3 *= 1 + l3 * _NoiseAmplitude;
+
+            // Vertex position/normal modification
+            v.vertex.xyz = v_idx == 0 ? v1 : (v_idx == 1 ? v2 : v3);
+            v.normal = normalize(cross(v2 - v1, v3 - v2));
+
+            // Transform modification
+            unity_ObjectToWorld = _LocalToWorld;
+            unity_WorldToObject = _WorldToLocal;
+        }
+
+        void surf(Input IN, inout SurfaceOutputStandard o)
+        {
+            o.Albedo = _Color.rgb;
+            o.Metallic = _Metallic;
+            o.Smoothness = _Smoothness;
+            o.Normal = float3(0, 0, IN.vface < 0 ? -1 : 1); // back face support
+        }
+
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
